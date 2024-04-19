ac296ae2327ca9d0715bf8dd57504749c605401a
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-04-19 17:31:02 +0100

    Create home page view controller. Create ui test file for the homepage view controller. Create unit test file for the homepage view controller.

---
+++ CryptoTicker/Controllers/CryptoTickerHomePageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CryptoTickerViewController.swift
+//  CryptoTicker
+//
+//  Created by David  Lourenço on 19/04/2024.
+//
+
+import Foundation
+import UIKit
+
+class CryptoTickerHomePageViewController : UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
---
+++ UnitTests/CryptoTickerHomePageUnitTests.swift
@@ -0,0 +1,24 @@
+//
+//  CryptoTickerHomePageUnitTests.swift
+//  CryptoTickerTests
+//
+//  Created by David  Lourenço on 19/04/2024.
+//
+
+import XCTest
+
+final class CryptoTickerHomePageUnitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+
+    }
+
+}
