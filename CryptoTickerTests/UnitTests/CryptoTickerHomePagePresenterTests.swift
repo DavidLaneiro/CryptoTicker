cebabea744b8a2106799d97b817eb318756465a6
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-04-20 16:20:20 +0100

    Create new files for the Unit Tests. Improve file organization.

---
+++ CryptoTickerTests/UnitTests/CryptoTickerHomePagePresenterTests.swift
@@ -0,0 +1,22 @@
+//
+//  CryptoTickerHomePagePresenterTests.swift
+//  CryptoTickerTests
+//
+//  Created by David  Lourenço on 20/04/2024.
+//
+
+import XCTest
+
+// MARK: Unit Tests for the PRESENTER
+final class CryptoTickerHomePagePresenterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+}
