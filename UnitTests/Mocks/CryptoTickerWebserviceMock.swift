08c9b606bf5babaffada7eb5daae646de31263f2
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-04-20 15:41:32 +0100

    Finish the mock version of Crypto Web Service.

--- UnitTests/Mocks/CryptoTickerWebserviceMock.swift
+++ UnitTests/Mocks/CryptoTickerWebserviceMock.swift
@@ -10,9 +10,28 @@ import Foundation
 // MARK: Mock version
 class CryptoTickerWebserviceMock : CryptoTickerWebserviceProtocol{
     
+    var isGetCoinsCalled : Bool = false
+    var shouldReturnError : Bool = false
     
     func getCoins(completionHandler: @escaping (CryptoTickerCoins?, CryptoTickerErrorModel?) -> Void) {
-        // Do something later
+        
+        isGetCoinsCalled = true
+        
+        if shouldReturnError{
+            
+            completionHandler(nil, CryptoTickerErrorModel.failedRequest(description: "The Get Coins request was not successful"))
+            
+        }else{
+            
+            // Dummy data
+            let cryptoTickerCoinExample = CryptoTickerCoin(id: "barbadian-dollar", symbol: "BBD", currencySymbol: "$", type:  "fiat", rateUsd: "0.5000000000000000")
+            
+            let successfulResponseModel = CryptoTickerCoins(data: [cryptoTickerCoinExample])
+            
+            completionHandler(successfulResponseModel, nil)
+            
+        }
+        
     }
 
     
