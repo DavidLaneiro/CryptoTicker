90f0564e4af102d8990a305b9ff15c888cdafefc
David Lourenço <david.alexandre.laneiro.lourenco@gmail.com>
2024-04-22 14:00:48 +0100

    Organize the project. Change the tests files.

--- CryptoTickerTests/UnitTests/Network/CryptoTickerWebServiceTests.swift
+++ CryptoTickerTests/UnitTests/Network/CryptoTickerWebServiceTests.swift
@@ -1,14 +1,16 @@
 //
-//  CryptoTickerHomePagePresenterTests.swift
+//  CryptoTickerWebServiceTests.swift
 //  CryptoTickerTests
 //
 //  Created by David  Lourenço on 20/04/2024.
 //
 
 import XCTest
+@testable import CryptoTicker
 
-// MARK: Unit Tests for the PRESENTER
-final class CryptoTickerHomePagePresenterTests: XCTestCase {
+
+// MARK: Unit Tests for the WebService
+final class CryptoTickerWebServiceTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,6 +19,4 @@ final class CryptoTickerHomePagePresenterTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-
 }
--- CryptoTickerTests/UnitTests/Presenters/CryptoTickerHomePagePresenterTests.swift
+++ CryptoTickerTests/UnitTests/Presenters/CryptoTickerHomePagePresenterTests.swift
@@ -1,15 +1,15 @@
 //
-//  CryptoTickerWebServiceTests.swift
+//  CryptoTickerHomePagePresenterTests.swift
 //  CryptoTickerTests
 //
 //  Created by David  Lourenço on 20/04/2024.
 //
 
 import XCTest
+@testable import CryptoTicker
 
-
-// MARK: Unit Tests for the WebService
-final class CryptoTickerWebServiceTests: XCTestCase {
+// MARK: Unit Tests for the PRESENTER
+final class CryptoTickerHomePagePresenterTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,4 +18,6 @@ final class CryptoTickerWebServiceTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+
+
 }
